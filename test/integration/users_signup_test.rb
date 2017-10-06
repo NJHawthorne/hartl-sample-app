3caadfa6e7bb2cfa0d1e590d2c1dd1e003b9d620
Nate Hawthorne <natejhawthorne@gmail.com>
2017-10-06 15:30:14 -0500

    Successfully redirects after signup and solved weird test issue

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,7 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with errors'
+    assert_select 'div.alert'
+    assert_select 'div#error_explanation'
   end
 end
